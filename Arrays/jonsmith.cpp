56dd085c1bec2f900994eb98b67fd72d57a77122
Avichal Kumar <105901472+Avichal14@users.noreply.github.com>
2023-03-09 21:00:02 +0530

    Add files via upload

---
+++ Arrays/jonsmith.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+     // string a;
+     // cin >> a;
+     // cout << a << endl;
+     // char f[20], l[20];
+     // cin >> f >> l;
+     // cout << f << " " << l << endl;
+     char a[] = { 'j','o','h','n' };
+     char b[] = { 's','m','i','t','h' };
+     char c[40];
+
+
+
+}
