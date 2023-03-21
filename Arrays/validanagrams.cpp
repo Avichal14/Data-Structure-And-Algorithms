6ed8c9ffebfa60abc90cf733595c1b3385133e83
Avichal Kumar <105901472+Avichal14@users.noreply.github.com>
2023-03-21 22:37:20 +0530

    Add files via upload

---
+++ Arrays/validanagrams.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+     string s;
+     cin >> s;
+     string t;
+     cin >> t;
+     sort(s.begin(), s.end());
+     sort(t.begin(), t.end());
+     if (s == t) {
+          cout << "Valid";
+     }
+     else {
+          cout << "INvalid";
+     }
+}
