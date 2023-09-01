e0ff41a9fe2a5f00f4290540a4ad8aa44fad72d9
Avichal Kumar <105901472+Avichal14@users.noreply.github.com>
2023-09-01 21:25:33 +0530

    Loop

--- Loops/main.cpp
+++ Loops/main.cpp
@@ -2,62 +2,21 @@
 using namespace std;
 
 int main() {
+  int n;
+  cin>>n;
+  for(int row=0;row<n;row++){
+    //space
+    for(int col=0;col<n-row-1;col++){
+      cout<<" ";
 
-  //****************************** Pattern Printing ********************************
-  // ===============================hollow rectangle=================================
- // int row,col;
- // cin>>row>>col; 
- //   for(int i=0;i<row;i++){
- //     for(int j=0;j<col;j++){
- //       if(i==0 || i==row-1){
- //         cout<<"* ";
- //       }
- //       else{
- //         if(j==0 || j==col-1){
- //           cout<<"* ";
- //         }
- //         else{
- //           cout<<"  ";
- //         }
- //       }
-      
- //     }
- //     cout<<endl;
-  
- //   }
+    }
 
-
- // ====================================Half Pyramid==========================================
- int n;
- cin>>n;
-//  for(int row=0;row<n;row=row+1){
-//   for(int col=0;col<n-row;col++){
-//     cout<<"* ";
-//   }
-//   cout<<endl;
-//  }
-//==================================== numerical half pyramid===================================
- for( int i=0;i<n;i++){
-  for(int j=0;j<i+1;j++){
-    cout<<j+1<<" ";
+    //star
+    for(int col=0;col<row+1;col++){
+      cout<<"* ";
+    }
+    cout<<endl;
   }
-  cout<<endl;
- }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   return 0;
 }
